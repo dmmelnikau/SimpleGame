559ad65ae9e4aaac8a22734d402672a49e0dc740
okmij <okmij@DESKTOP-26N6QAU>
2021-09-10 18:21:20 +0300

    Refactoring

--- Game/Rules.cs
+++ Game/Rules.cs
@@ -23,10 +23,8 @@ namespace Game
                 var generator = new Random();
                 var randomChoice = generator.Next(1, arg_cmd.Length);
                 Message = "Computer move:" + arg_cmd[randomChoice - 1];
-                string hmac_hex_key = HMACkey.HmacSha256(Key.Gen(), Rules.Message);
-                
-                Console.WriteLine("HMAC:" + hmac_hex_key);
-                Console.WriteLine("Avaliable moves:");
+                string hmac_hex_key = HMACkey.HmacSha256(Key.Gen(), Rules.Message);           
+                Console.WriteLine("HMAC:" + hmac_hex_key+"\n"+"Avaliable moves:");      
                 for (int i = 0; i < arg_cmd.Length; i++)
                 {
                     Console.WriteLine("{0}-{1}", i + 1, arg_cmd[i]);
@@ -38,10 +36,10 @@ namespace Game
                     if (selection == 0) return;
                     else if ((selection <= arg_cmd.Length) && selection > 0)
                     {
-                        Console.WriteLine("Your move:{0}", arg_cmd[selection - 1]);
+                        Console.WriteLine("Your move:{0}\nComputer move:{0}", arg_cmd[selection - 1], arg_cmd[randomChoice - 1]);
                        
                     
-                         Console.WriteLine("Computer move:{0}", arg_cmd[randomChoice - 1]);
+                        
                       //  Console.WriteLine(Message);
                         if (selection == randomChoice) Console.WriteLine("Draw");
                         if ((selection < randomChoice) && ((selection + arg_cmd.Length / 2.0) < randomChoice))
