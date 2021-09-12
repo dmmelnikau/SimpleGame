50955afe90ded192b2f0df3724c23277d7304d0c
okmij <okmij@DESKTOP-26N6QAU>
2021-09-12 14:31:45 +0300

    RandomNumberGenerator.GetInt32

--- Game/Rules.cs
+++ Game/Rules.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -30,12 +31,12 @@ namespace Game
         }
         public static void Main_Rules( string[] arg_cmd, int selection, int randomChoice)
         {
-            Console.WriteLine("Your move:{0}\nComputer move:{1}", arg_cmd[selection - 1], arg_cmd[randomChoice - 1]);
-            if (selection == randomChoice) Console.WriteLine("Draw");
-            if ((selection < randomChoice) && ((selection + arg_cmd.Length / 2.0) < randomChoice)) { Console.WriteLine("You lose"); }
-            if ((selection < randomChoice) && (selection + arg_cmd.Length / 2.0) > randomChoice) { Console.WriteLine("You Win"); }
-            if ((selection > randomChoice) && ((randomChoice + arg_cmd.Length / 2.0) < selection)) { Console.WriteLine("You Win"); }
-            if ((selection > randomChoice) && ((randomChoice + arg_cmd.Length / 2.0) > selection)) { Console.WriteLine("You lose"); }
+            Console.WriteLine("Your move:{0}\nComputer move:{1}", arg_cmd[selection - 1], arg_cmd[randomChoice]);
+            if (selection == randomChoice + 1) Console.WriteLine("Draw");
+            if ((selection < randomChoice + 1) && ((selection + arg_cmd.Length / 2.0) < (randomChoice + 1))) { Console.WriteLine("You lose"); }
+            if ((selection < randomChoice + 1) && (selection + arg_cmd.Length / 2.0) > (randomChoice + 1)) { Console.WriteLine("You Win"); }
+            if ((selection > randomChoice + 1) && (((randomChoice + 1)+ arg_cmd.Length / 2.0) < selection)) { Console.WriteLine("You Win"); }
+            if ((selection > randomChoice + 1) && (((randomChoice + 1) + arg_cmd.Length / 2.0) > selection)) { Console.WriteLine("You lose"); }
             Console.WriteLine("HMAC key:{0}", Key.Gen());
         }
         public static void Moves_in_game(string[] arg_cmd)
@@ -49,14 +50,15 @@ namespace Game
         public static void New_Game(string[] arg_cmd)
         {
             Console.WriteLine("New Game");
-            var generator = new Random();
-            var randomChoice = generator.Next(1, arg_cmd.Length);
-            Message = "Computer move:" + arg_cmd[randomChoice - 1];
-            string hmac_hex_key = HMACkey.HmacSha256(Key.Gen(), Rules.Message);
-            Console.WriteLine("HMAC:" + hmac_hex_key + "\n" + "Avaliable moves:");
-            for (int i = 0; i < arg_cmd.Length; i++) { Console.WriteLine("{0}-{1}", i + 1, arg_cmd[i]); }
-            Console.Write($"{arg_cmd.Length + 1}-help\r\n0-exit\r\nEnter your move:");
-            ConditionsOfGame(arg_cmd, randomChoice);
+           
+                int randomChoice = RandomNumberGenerator.GetInt32(arg_cmd.Length);
+                Message = "Computer move:" + arg_cmd[randomChoice];
+                string hmac_hex_key = HMACkey.HmacSha256(Key.Gen(), Rules.Message);
+                Console.WriteLine("HMAC:" + hmac_hex_key + "\n" + "Avaliable moves:");
+                for (int i = 0; i < arg_cmd.Length; i++) { Console.WriteLine("{0}-{1}", i + 1, arg_cmd[i]); }
+                Console.Write($"{arg_cmd.Length + 1}-help\r\n0-exit\r\nEnter your move:");
+                ConditionsOfGame(arg_cmd, randomChoice);
+           
         }
     }
 }
